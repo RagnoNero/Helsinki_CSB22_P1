c04dae619f43f490d88b0aa458dbb9272decc017
RagnoNero <vladyslav.nikolaichuk@nice.com>
2022-12-22 22:18:13 +0200

    Added dummy code for notes

--- notes/urls.py
+++ notes/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from django.contrib.auth.views import LogoutView
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('login/', views.login_user, name='login_user'),
-	path('logout/', LogoutView.as_view(next_page='/login/'))
+	path('logout/', LogoutView.as_view(next_page='/login/')),
+    path('allNotes/', views.view_notes, name='allNotes'),
+    path('addNote/', views.add_note, name='addNote'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from .forms import LoginForm, RegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .models import Note
 
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
 
 def register(request):
@@ -32,7 +34,43 @@ def login_user(request):
         user = User.objects.filter(username=username, password=password).first()
         if user is not None:
             login(request, user)
-            return redirect('/index')
+            return redirect('/allNotes')
 
     form = LoginForm()
     return render(request, 'login.html', {'form': form})
+
+
+@login_required
+def add_note(request):
+    if request.method == 'POST':
+        note_id = int(request.POST.get('id', 0))
+        user_id = request.user.id
+        title = request.POST.get('title')
+        content = request.POST.get('content', '')
+
+        if note_id > 0:
+            document = Note.objects.get(pk=note_id)
+            document.title = title
+            document.content = content
+            document.save()
+
+            return redirect('/?id=%i' % note_id)
+        else:
+            document = Note.objects.create(title=title, content=content, user_id=user_id)
+
+            return redirect('/?id=%i' % document.id)
+    return render(request, 'addNote.html')
+
+
+@login_required
+def view_notes(request):
+    notes = Note.objects.filter(user_id=request.user.id)
+    return render(request, 'allNotes.html', notes)
+
+
+@login_required
+def remove_note(request):
+    note_id = request.POST.get('remove')
+    note = Note.objects.get(id=note_id)
+    note.delete()
+    return redirect('/allNotes')
